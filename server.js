fe78d37310d34ab67caa3fa44bf0fc94bade5a86
Kelsey Alderman <kelseyalderman@gmail.com>
2022-04-11 17:41:08 -0700

    query the database

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+// Import mysql2 package
+const mysql = require("mysql2");
+
 // Import express
 const express = require("express");
-const { reset } = require("yargs");
 
 // Port designation & app expression
 const PORT = process.env.PORT || 3001;
@@ -9,6 +11,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect application to the MySQL database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username (default is root)
+    user: "root",
+    // MySQL password (default is no password)
+    password: "",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+// Return all data in the candidates table
+db.query("SELECT * FROM candidates", (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request- user requests that aren't supported by the app- (Not Found)
 app.use((req, res) => {
