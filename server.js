327691b32e32d0a2d9423dc2fb4f63c73d299789
Kelsey Alderman <kelseyalderman@gmail.com>
2022-04-11 18:11:19 -0700

    build the database calls

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 
 // Import express
 const express = require("express");
+const { ConsoleWriter } = require("istanbul-lib-report");
 
 // Port designation & app expression
 const PORT = process.env.PORT || 3001;
@@ -25,9 +26,37 @@ const db = mysql.createConnection(
   console.log("Connected to the election database.")
 );
 
-// Return all data in the candidates table
-db.query("SELECT * FROM candidates", (err, rows) => {
-  console.log(rows);
+// // Return all data in the candidates table
+// db.query("SELECT * FROM candidates", (err, rows) => {
+//   console.log(rows);
+// });
+
+// // GET a single candidate
+// db.query("SELECT * FROM candidates WHERE id = 1", (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// // Delete a candidate
+// db.query("DELETE FROM candidates WHERE id = ?", 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+// Create a candidate
+const sql =
+  "INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)";
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
 });
 
 // Default response for any other request- user requests that aren't supported by the app- (Not Found)
